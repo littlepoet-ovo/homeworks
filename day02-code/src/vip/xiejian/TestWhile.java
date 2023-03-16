574948f86e3013e428bf683b463fa66afd84f6c2
littlepoet-ovo <113732483+littlepoet-ovo@users.noreply.github.com>
2023-03-16 19:48:36 +0800

    Delete day02-code directory
    Signed-off-by: littlepoet-ovo <113732483+littlepoet-ovo@users.noreply.github.com>

--- day02-code/src/vip/xiejian/TestWhile.java
+++
@@ -1,9 +0,0 @@
-package vip.xiejian;
-
-public class TestWhile {
-    public static void main(String[] args) {
-        for(int i=0,j=0;i<10;i++,j++){
-            System.out.println(i+" "+j);
-        }
-    }
-}
