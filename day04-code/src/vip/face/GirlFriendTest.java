aa76cbe9971b7014d09707275498a73f3973da11
littlepoet-ovo <113732483+littlepoet-ovo@users.noreply.github.com>
2023-03-16 19:48:56 +0800

    Delete day04-code directory
    Signed-off-by: littlepoet-ovo <113732483+littlepoet-ovo@users.noreply.github.com>

--- day04-code/src/vip/face/GirlFriendTest.java
+++
@@ -1,9 +0,0 @@
-package vip.face;
-
-public class GirlFriendTest {
-    public static void main(String[] args) {
-        GirlFriend p = new GirlFriend(18,"高蝶");
-        System.out.println(p.name);
-        System.out.println(p.getAge());
-    }
-}
